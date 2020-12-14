05179e661f094edc74d41c86dacaadde7988fdd7
albajimeneza3 <76017404+albajimeneza3@users.noreply.github.com>
2020-12-15 00:30:42 +0100

    Create hotel lambda localisation.js

---
+++ hotel lambda localisation.js
@@ -0,0 +1,20 @@
+module.exports = {
+    es: {
+        translation: {
+            WELCOME_MSG: '¡Bienvenido! Dime, ¿Cuándo es la fecha de reserva de tu hotel? ',
+            REGISTER_MSG: 'Recordaré que tu fecha de reserva de hotel es el %s de %s de %s. ',
+            SAY_MSG: 'Quedan %s días para que se cumpla la fecha de reserva de tu hotel %s.',
+            GREET_MSG: '¡Hoy es la reserva del hotel que tenías previsto!',
+            MISSING_MSG: 'Parece que aun no me has dicho tu fecha de reserva de hotel. Prueba decir, quiero reservar hotel. ',
+            OVERWRITE_MSG: 'Si quieres cambiar la fecha solo di, quiero reservar hotel',
+            HELP_MSG: 'Puedes preguntarme cuanto falta para la reserva de tu hotel. O registrar la fecha diciendo, quiero reservar hotel.',
+            GOODBYE_MSG: '¡Hasta luego! ',
+            OFERTA_MSG: 'Si no sabes que hotel elegir entra en el link: trivago.es o booking.com',
+            NOMBREHOTEL_MSG: 'Se ha registrado tu hotel %s. Si quieres registrar fecha di reservar hotel.',
+            SAYHOTEL_MSG: 'El nombre del hotel que has reservado es %s. Si quieres saber la fecha di cuando es mi reserva de hotel.',
+            REFLECTOR_MSG: 'Acabas de activar %s ',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez. ',
+            ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez. '
+        }
+    }
+}
