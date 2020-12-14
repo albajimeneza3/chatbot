fb8017316948a85bc9611db936a11093ffbb6e9e
albajimeneza3 <76017404+albajimeneza3@users.noreply.github.com>
2020-12-15 00:27:10 +0100

    Create vuelo lambda localisation.js

---
+++ vuelo lambda localisation.js
@@ -0,0 +1,17 @@
+module.exports = {
+    es: {
+        translation: {
+            WELCOME_MSG: '¡Bienvenido! Dime, ¿Cuándo es la fecha de reserva de tu vuelo? ',
+            REGISTER_MSG: 'Recordaré que tu fecha de reserva es el %s de %s de %s. ',
+            SAY_MSG: 'Quedan %s días para que se cumpla la fecha de reserva.',
+            GREET_MSG: '¡Hoy es la reserva del vuelo que tenías previsto!',
+            MISSING_MSG: 'Parece que aun no me has dicho tu fecha de reserva. Prueba decir, quiero reservar. ',
+            OVERWRITE_MSG: 'Si quieres cambiar la fecha solo di, quiero reservar',
+            HELP_MSG: 'Puedes preguntarme cuanto falta para la reserva. O registrar la fecha diciendo, quiero reservar.',
+            GOODBYE_MSG: '¡Hasta luego! ',
+            REFLECTOR_MSG: 'Acabas de activar %s ',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez. ',
+            ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez. '
+        }
+    }
+}
